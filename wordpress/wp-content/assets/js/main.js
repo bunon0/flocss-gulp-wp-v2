de86f983290740de266c8adc55d9e98baf6b210d
bunon0 <86172592+bunon0@users.noreply.github.com>
2022-09-12 09:10:35 +0900

    theme-nameディレクトリの追加

---
+++ wordpress/wp-content/assets/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const test = () => {
+  console.log("main.jsです");
+};
+
+test();
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ wordpress/wp-content/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- icon-->
+  <link rel="icon" href="<?php echo esc_url(get_theme_file_uri("/assets/images/favicon.ico")); ?>" />
+  <link rel="shortcut icon" href="" type="image/x-icon">
+  <link rel="apple-touch-icon" href="./apple-touch-icon.png">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class=""></header>
+
+  <main>
+    <?php wp_body_open(); ?>
