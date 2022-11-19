0373b371065115de50ab76cb92bdebdd7c7ce5ae
bunon0 <86172592+bunon0@users.noreply.github.com>
2022-11-19 09:38:00 +0900

    ビルドテスト #21

--- wordpress/wp-content/themes/theme-name/assets/js/main.js
+++ wordpress/wp-content/themes/theme-name/assets/js/main.js
@@ -1,18 +1,1 @@
-"use strict";
-
-const a = () => {
-  console.log("a");
-};
-
-const b = () => {
-  console.log("b");
-};
-
-const test = () => {
-  console.log("main.jsです");
-  a();
-  b();
-};
-
-test();
-//# sourceMappingURL=sourcemaps/main.js.map
+"use strict";const a=()=>{console.log("a")},b=()=>{console.log("b")},test=()=>{console.log("main.jsです"),a(),b()};test();
